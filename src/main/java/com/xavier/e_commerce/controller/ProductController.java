2557c19e96b14a0370c6135260c64ed000fc106c
Luiz Xavier <luizxsantos0@gmail.com>
2025-07-04 19:23:22 -0300

    update product controller

--- src/main/java/com/xavier/e_commerce/controller/ProductController.java
+++ src/main/java/com/xavier/e_commerce/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.xavier.e_commerce.controller;
 
 import com.xavier.e_commerce.DTO.CreateProductDTO;
+import com.xavier.e_commerce.entity.Category;
 import com.xavier.e_commerce.entity.Product;
 import com.xavier.e_commerce.service.AddProductsService;
 import com.xavier.e_commerce.service.GetProductsService;
+import com.xavier.e_commerce.service.UpdateProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("products")
@@ -19,10 +23,23 @@ public class ProductController {
     @Autowired
     GetProductsService getProductsService;
 
+    @Autowired
+    UpdateProductService updateProductService;
+
     @PostMapping("")
     public String addProduct(@RequestBody CreateProductDTO product){
         addProductService.addProduct(product);
         return "Product succesfully added!";
+    }
+
+    @GetMapping("{id}")
+    public Optional<Product> getProductById(@PathVariable UUID id){
+        return getProductsService.getProductById(id);
+    }
+
+    @GetMapping("{category}")
+    public List<Product> getProductsByCategory(@PathVariable Category category){
+        return getProductsService.getProductsByCategory(category);
     }
 
     @GetMapping
@@ -30,5 +47,10 @@ public class ProductController {
         return getProductsService.getAllProducts();
     }
 
+    @PutMapping("id")
+    public String updateProduct(@PathVariable UUID id, @RequestBody CreateProductDTO productDTO){
+        updateProductService.updateProduct(id, productDTO);
+        return "Product was updated succesfully";
+    }
 
 }
