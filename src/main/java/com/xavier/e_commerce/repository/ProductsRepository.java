292f6623aca2572f2a66b17fd732bd1eb9ce06dc
Luiz Xavier <luizxsantos0@gmail.com>
2025-07-04 19:21:29 -0300

    feat: new repository method

--- src/main/java/com/xavier/e_commerce/repository/ProductsRepository.java
+++ src/main/java/com/xavier/e_commerce/repository/ProductsRepository.java
@@ -1,12 +1,15 @@
 package com.xavier.e_commerce.repository;
 
+import com.xavier.e_commerce.entity.Category;
 import com.xavier.e_commerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, UUID> {
+    List<Product> findAllByCategory(Category category);
 
 }
